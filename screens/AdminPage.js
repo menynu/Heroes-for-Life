dd49d5a3f7b7e08df0cde214e93546df031e67ff
menynu <33396923+menynu@users.noreply.github.com>
2020-05-30 14:54:17 +0300

    added picker to connect db
    choose from existing delegation

--- screens/AdminPage.js
+++ screens/AdminPage.js
@@ -146,152 +146,151 @@ export default class AdminPage extends React.Component {
 
                 <ScrollView>
 
-                                <Card>
-                                    <Form>
-                                        <Text style={{fontSize:24}}> לרישום משתמש במערכת:</Text>
-                                        <Item floatingLabel>
-                                            <Label>אימייל</Label>
-                                            <Input
-                                                placeholder={'Test@test.com'}
-                                                value={this.state.email}
-                                                onChangeText={(val) => this.inputValueUpdate(val, 'email')}
-                                            />
+                    <Card>
+                        <Form>
+                            <Text style={{fontSize:24}}> לרישום משתמש במערכת:</Text>
+                            <Item floatingLabel>
+                                <Label>אימייל</Label>
+                                <Input
+                                    placeholder={'Test@test.com'}
+                                    value={this.state.email}
+                                    onChangeText={(val) => this.inputValueUpdate(val, 'email')}
+                                />
 
-                                        </Item>
-                                        <Item floatingLabel last>
-                                            <Label>סיסמא ראשונית</Label>
-                                            <Input
-                                                placeholder={'password'}
-                                                value={this.state.password}
-                                                onChangeText={(val) => this.inputValueUpdate(val, 'password')}
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label>סיסמא ראשונית</Label>
+                                <Input
+                                    placeholder={'password'}
+                                    value={this.state.password}
+                                    onChangeText={(val) => this.inputValueUpdate(val, 'password')}
 
-                                            />
-                                        </Item>
-                                        <Item floatingLabel last>
-                                            <Label>שם משלחת</Label>
-                                            <Input
-                                                placeholder={'india 2020'}
-                                                value={this.state.delegation}
-                                                onChangeText={(val) => this.inputValueUpdate(val, 'delegation')}
+                                />
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label>שם משלחת</Label>
+                                <Input
+                                    placeholder={'india 2020'}
+                                    value={this.state.delegation}
+                                    onChangeText={(val) => this.inputValueUpdate(val, 'delegation')}
 
-                                            />
-                                        </Item>
-                                        <Item floatingLabel last>
-                                            <Label>מלגאי/מנהל</Label>
-                                            <Input
-                                                placeholder={'M/V'}
-                                                value={this.state.permission}
-                                                onChangeText={(val) => this.inputValueUpdate(val, 'permission')}
-
-
-                                            />
-
-                                        </Item>
-                                        <Button iconLeft
-                                                onPress={() => this.addVolunteer()}
-                                        >
-                                            <Icon name='paper' />
-                                            <Text>הוספת משתמש חדש למערכת</Text>
-                                        </Button>
-                                    </Form>
-                                </Card>
-
-                                <Card>
-                                    <Form>
-                                        <Text style={{fontSize:24}}> להוספת משלחת:</Text>
-                                        <Item floatingLabel>
-                                            <Label>שם משלחת:</Label>
-                                            <Input
-                                                placeholder={'שם המשלחת'}
-                                                value={this.state.name}
-                                                onChangeText={(val) => this.delegationNameUpdate(val, 'name')}
-
-                                            />
-                                        </Item>
-                                        <Item >
-                                            <Text>תאריך סגירת המשלחת:</Text>
-                                            <DatePicker
-                                                defaultDate={new Date(2020, 8, 1)}
-                                                minimumDate={new Date(2020, 8, 1)}
-                                                maximumDate={new Date(2030, 12, 31)}
-                                                locale={"en"}
-                                                timeZoneOffsetInMinutes={undefined}
-                                                modalTransparent={false}
-                                                animationType={"fade"}
-                                                androidMode={"default"}
-                                                placeHolderText="Select date"
-                                                textStyle={{ color: "green" }}
-                                                placeHolderTextStyle={{ color: "#d3d3d3" }}
-                                                onDateChange={this.setDate}
-                                                disabled={false}
-                                            />
-                                        </Item>
-
-                                        <Button iconLeft
-                                                onPress={() => this.addDelegationName()}
-
-                                        >
-                                            <Icon name='paper' />
-                                            <Text >הוספת משלחת למערכת</Text>
-                                        </Button>
-                                    </Form>
-                                </Card>
-
-                                <Card>
-                                    <Form>
-                                        <Text style={{fontSize:24}}> לעדכון משלחת קיימת:</Text>
-                                        <Item floatingLabel>
-                                            <Label>בחר משלחת</Label>
-                                        </Item>
-                                        <Item picker>
-                                            <Picker
-                                                mode="dropdown"
-                                                style={{ width: 20 }}
-                                                placeholder="choose destination"
-                                                placeholderStyle={{ color: "#bfc6ea" }}
-                                                placeholderIconColor="#007aff"
-                                                selectedValue={this.state.delegation}
-                                                onValueChange={ (value) => ( this.setState({delegation: value}) ) } >
-                                                <Picker.Item label="בחר" value="" />
-                                                {
-                                                    this.state.destinationArr.map( (city, i) => {
-
-                                                        return <Picker.Item label={city.name} value={city.name} key={i} />
-                                                    })
-                                                }
-                                            </Picker>
-                                        </Item>
-
-                                        <Button iconLeft
-                                                onPress={() => this.addVolunteer()}
-                                        >
-                                            <Icon name='paper' />
-                                            <Text>הוספה למערכת</Text>
-                                        </Button>
-                                    </Form>
-
-                                </Card>
+                                />
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label>מלגאי/מנהל</Label>
+                                <Input
+                                    placeholder={'M/V'}
+                                    value={this.state.permission}
+                                    onChangeText={(val) => this.inputValueUpdate(val, 'permission')}
 
 
-                                <Card>
-                                        <Text style={{fontSize:24}}>הנפקת דוחות</Text>
-                                        <Button iconLeft onPress={() =>{this.props.navigation.navigate('Report1')}}>
-                                            <Icon name='paper' />
-                                            <Text>לקבלת דוח שבועי לפי משלחת לחץ כאן</Text>
-                                        </Button>
-                                        <Button iconLeft onPress={() => {this.props.navigation.navigate('Report2')}}>
-                                            <Icon name='paper' />
-                                            <Text>לקבלת דוח איזורים לפי משלחת לחץ כאן</Text>
-                                        </Button>
-                                        <Button iconLeft onPress={() => {this.props.navigation.navigate('Report3')}}>
-                                            <Icon name='paper' />
-                                            <Text>לקבלת דוח איזורים על איך שמעת על העמותה לחץ כאן</Text>
-                                        </Button>
-                                        <Button iconLeft onPress={()=>{this.props.navigation.navigate('Report4')}}>
-                                            <Icon name='paper' />
-                                            <Text>לקבלת דוח כללי כללי לחץ כאן</Text>
-                                        </Button>
-                                </Card>
+                                />
+
+                            </Item>
+                            <Button iconLeft
+                                    onPress={() => this.addVolunteer()}
+                            >
+                                <Icon name='paper' />
+                                <Text>הוספת משתמש חדש למערכת</Text>
+                            </Button>
+                        </Form>
+                    </Card>
+
+                    <Card>
+                        <Form>
+                            <Text style={{fontSize:24}}> להוספת משלחת:</Text>
+                            <Item floatingLabel>
+                                <Label>שם משלחת:</Label>
+                                <Input
+                                    placeholder={'שם המשלחת'}
+                                    value={this.state.name}
+                                    onChangeText={(val) => this.delegationNameUpdate(val, 'name')}
+
+                                />
+                            </Item>
+                            <Item >
+                                <Text>תאריך סגירת המשלחת:</Text>
+                                <DatePicker
+                                    defaultDate={new Date(2020, 8, 1)}
+                                    minimumDate={new Date(2020, 8, 1)}
+                                    maximumDate={new Date(2030, 12, 31)}
+                                    locale={"en"}
+                                    timeZoneOffsetInMinutes={undefined}
+                                    modalTransparent={false}
+                                    animationType={"fade"}
+                                    androidMode={"default"}
+                                    placeHolderText="Select date"
+                                    textStyle={{ color: "green" }}
+                                    placeHolderTextStyle={{ color: "#d3d3d3" }}
+                                    onDateChange={this.setDate}
+                                    disabled={false}
+                                />
+                            </Item>
+
+                            <Button iconLeft
+                                    onPress={() => this.addDelegationName()}
+
+                            >
+                                <Icon name='paper' />
+                                <Text >הוספת משלחת למערכת</Text>
+                            </Button>
+                        </Form>
+                    </Card>
+
+                    <Card>
+                        <Form>
+                            <Text style={{fontSize:24}}> לעדכון משלחת קיימת:</Text>
+                            <Item floatingLabel>
+                                <Label>בחר משלחת</Label>
+                            </Item>
+                            <Item picker>
+                                <Picker
+                                    mode="dropdown"
+                                    style={{ width: 20 }}
+                                    placeholder="choose destination"
+                                    placeholderStyle={{ color: "#bfc6ea" }}
+                                    placeholderIconColor="#007aff"
+                                    selectedValue={this.state.delegation}
+                                    onValueChange={ (value) => ( this.setState({delegation: value}) ) } >
+                                    <Picker.Item label="בחר" value="" />
+                                    {
+                                        this.state.destinationArr.map( (city, i) => {
+
+                                            return <Picker.Item label={city.name} value={city.name} key={i} />
+                                        })
+                                    }
+                                </Picker>
+                            </Item>
+
+                            <Button iconLeft
+                                    onPress={() => this.addVolunteer()}
+                            >
+                                <Icon name='paper' />
+                                <Text>הוספה למערכת</Text>
+                            </Button>
+                        </Form>
+
+                    </Card>
+
+                    <Card>
+                        <Text style={{fontSize:24}}>הנפקת דוחות</Text>
+                        <Button iconLeft onPress={() =>{this.props.navigation.navigate('Report1')}}>
+                            <Icon name='paper' />
+                            <Text>לקבלת דוח שבועי לפי משלחת לחץ כאן</Text>
+                        </Button>
+                        <Button iconLeft onPress={() => {this.props.navigation.navigate('Report2')}}>
+                            <Icon name='paper' />
+                            <Text>לקבלת דוח איזורים לפי משלחת לחץ כאן</Text>
+                        </Button>
+                        <Button iconLeft onPress={() => {this.props.navigation.navigate('Report3')}}>
+                            <Icon name='paper' />
+                            <Text>לקבלת דוח איזורים על איך שמעת על העמותה לחץ כאן</Text>
+                        </Button>
+                        <Button iconLeft onPress={()=>{this.props.navigation.navigate('Report4')}}>
+                            <Icon name='paper' />
+                            <Text>לקבלת דוח כללי כללי לחץ כאן</Text>
+                        </Button>
+                    </Card>
 
                 </ScrollView>
 
@@ -312,4 +311,3 @@ const styles = StyleSheet.create({
         backgroundColor: "red",
     }
 });
-
